c6fe79e442e6f16feaf7c458ec790ad17c509b0f
kolkol <kolodiy1103@gmail.com>
2020-07-04 18:14:19 +0200

    sending emails v1.0

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const cors = require("cors");
+const bodyParser = require('body-parser')
 const passwordsRouter = require("./routes/passwords");
 const userRouter = require("./routes/User");
 // const couponRouter = require("./routes/Coupon");
@@ -23,6 +24,10 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
 
 app.use(cors());
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
