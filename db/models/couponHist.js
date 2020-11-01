2bfa7bb75487decd5f76c80eb1e73e3f9b9cd184
kolkol <kolodiy1103@gmail.com>
2020-11-01 19:04:49 +0100

    add mongoose type of timestamp

--- db/models/couponHist.js
+++ db/models/couponHist.js
@@ -1,12 +1,14 @@
 const mongoose = require("mongoose");
 const { Timestamp } = require("mongodb");
-const couponHistSchema = new mongoose.Schema({
-  couponID: { type: mongoose.ObjectId, require: true }, //coupon ID
-  userID: { type: mongoose.ObjectId, require: true }, //user ID
-  timestamp: { type: Timestamp, require: true }, //timestamp of action
-  prevStatus: { type: String, require: true }, //previous status
-  newStatus: { type: String, require: true }, //new status
-});
+const couponHistSchema = new mongoose.Schema(
+  {
+    couponID: { type: mongoose.ObjectId, require: true }, //coupon ID
+    userID: { type: mongoose.ObjectId, require: true }, //user ID
+    prevStatus: { type: String, require: true }, //previous status
+    newStatus: { type: String, require: true }, //new status
+  },
+  { timestamps: true }
+);
 const CouponHistory = mongoose.model("CouponHistory", couponHistSchema);
 
 module.exports = CouponHistory;
