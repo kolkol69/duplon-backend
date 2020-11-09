d0b1a6445f94f15f18306dea6c0abedaf63a4970
kolkol <kolodiy1103@gmail.com>
2020-11-09 21:09:28 +0100

    upd tenant Scheme field

--- db/models/tenant.js
+++ db/models/tenant.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const tenantSchema = new mongoose.Schema({
 	name: { type: String, require: true },
-	active: { type: Date, require: true },
+	activeStatus: { type: Date, require: true },
 })
 const Tenant = mongoose.model('Tenant', tenantSchema)
 
