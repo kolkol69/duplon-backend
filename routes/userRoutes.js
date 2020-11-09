d544e29efd9c27cad4ca27915aba1e0efd7a754b
kolkol <kolodiy1103@gmail.com>
2020-11-09 19:51:37 +0100

    add new route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,9 +2,10 @@ const express = require('express')
 const router = express.Router()
 const userController = require('../controllers/userController')
 
-const { getAllUsers, createUser } = userController
+const { getAllUsers, createUser, getAllTenantUsers } = userController
 
 router.route('/').get(getAllUsers).post(createUser)
+router.route('/:tenantId').get(getAllTenantUsers)
 // router
 //   .route('/:tenantId/:userId')
 //   .get(getUser)
