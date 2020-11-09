4fe4f4b08babbaa9b581cf3ba435f1d59c3c571b
kolkol <kolodiy1103@gmail.com>
2020-11-09 19:37:38 +0100

    add coupon usage scheme

---
+++ db/models/couponUsage.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+
+const { Schema, SchemaTypes } = mongoose
+const { ObjectId, Number } = SchemaTypes
+const couponUsageSchema = new Schema({
+  tenantID: { type: ObjectId, require: true },
+  usedAll: { type: Number, require: true }, //previous status
+  usedLastPeriod: { type: Number, require: true }, //new status
+})
+const CouponUsage = mongoose.model('CouponUsage', couponUsageSchema)
+
+module.exports = CouponUsage
