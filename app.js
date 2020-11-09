9cd0ec265b00e113c6b5ddfd6eb1f14c02cc6969
kolkol <kolodiy1103@gmail.com>
2020-11-10 00:59:06 +0100

    refactoring + change for root navigation

--- app.js
+++ app.js
@@ -15,12 +15,6 @@ require('./db/mongoose')
 
 const app = express()
 
-// show BE readme
-require('express-readme')(app, {
-  filename: 'readme.md',
-  routes: ['/', '/readme']
-})
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'jade')
@@ -43,24 +37,13 @@ app.use('/send-email', emailRouter)
 app.use('/api/v1/users', userRouter)
 app.use('/api/v1/tenants', tenantRouter)
 // app.use("/coupon", couponRouter);
-
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'client/build')))
-
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404))
+app.use('/', (req, res) => {
+  res.redirect(process.env.API_DOCUMENTATION)
 })
 
-// error handler
-app.use((err, req, res) => {
-  // set locals, only providing error in development
-  res.locals.message = err.message
-  res.locals.error = req.app.get('env') === 'development' ? err : {}
-
-  // render the error page
-  res.status(err.status || 500)
-  res.render('error')
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  next(createError(404))
 })
 
 module.exports = app
