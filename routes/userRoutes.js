e7221f1c57765fb25714b468acf753d115e34919
kolkol <kolodiy1103@gmail.com>
2020-11-09 21:10:31 +0100

    uncomment routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,14 +2,21 @@ const express = require('express')
 const router = express.Router()
 const userController = require('../controllers/userController')
 
-const { getAllUsers, createUser, getAllTenantUsers } = userController
+const {
+  getAllUsers,
+  createUser,
+  getAllTenantUsers,
+  getUser,
+  updateUser,
+  deleteUser,
+} = userController
 
 router.route('/').get(getAllUsers).post(createUser)
 router.route('/:tenantId').get(getAllTenantUsers)
-// router
-//   .route('/:tenantId/:userId')
-//   .get(getUser)
-//   .patch(updateUser)
-//   .delete(deleteUser)
+router
+  .route('/:tenantId/:userId')
+  .get(getUser)
+  .patch(updateUser)
+  .delete(deleteUser)
 
 module.exports = router
