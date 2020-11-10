231b4779353c8a1359898d88cdd371bb5ca099de
kolkol <kolodiy1103@gmail.com>
2020-11-10 12:39:39 +0100

    minor change

--- models/tenantModel.js
+++ models/tenantModel.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const tenantSchema = new mongoose.Schema({
   name: { type: String, require: true, unique: true },
   activeStatus: { type: Date },
-  joinDate: { type: Date, default: new Date() }
+  joinDate: { type: Date, default: Date.now() }
 })
 const Tenant = mongoose.model('Tenant', tenantSchema)
 
