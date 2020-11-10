ce442cd1392193c075f03954a38e5325062bcf95
kolkol <kolodiy1103@gmail.com>
2020-11-10 13:56:48 +0100

    add sort, pagination, fields

--- controllers/tenantController.js
+++ controllers/tenantController.js
@@ -1,13 +1,21 @@
 const Tenant = require('../models/tenantModel')
 const User = require('../models/userModel')
 
+const DEFAULT_LIMIT = 10
+const DEFAULT_PAGE = 1
+const DEFAULT_SORT = '-joinDate'
+const USER_ACCESS = {
+  FULL: 'full',
+  DEFAULT: 'default'
+}
+
 exports.createTenant = async (req, res) => {
   try {
     await Tenant.create(req.body).then(async ({ _id, name }) => {
       try {
         await User.create({
           tenantId: _id,
-          access: 'full', // oneOf[full, default]
+          access: USER_ACCESS.FULL,
           login: `${name.replace(/\s/g, '')}_login`,
           password: `${name.replace(/\s/g, '')}_password`
         }).then(() => res.status(200).json({ status: 'success' }))
@@ -26,20 +34,44 @@ exports.createTenant = async (req, res) => {
   }
 }
 
+const filter = (filterOptions) => {
+  const queryObject = { ...filterOptions }
+  const excludeFileds = ['page', 'sort', 'limit', 'fields']
+
+  excludeFileds.forEach((field) => delete queryObject[field])
+  return JSON.parse(
+    JSON.stringify(queryObject).replace(
+      /(gt|gte|lt|gt)/g,
+      (match) => `$${match}`
+    )
+  )
+}
+const sort = (sortOptions) =>
+  sortOptions ? sortOptions.split(',').join(' ') : DEFAULT_SORT
+
+const chooseFields = (fieldOptions) =>
+  fieldOptions ? fieldOptions.split(',').join(' ') : '-__v'
+
+const pagination = (query) => {
+  const page = +query.page || DEFAULT_PAGE
+  const limit = +query.limit || DEFAULT_LIMIT
+  const skip = (page - 1) * limit
+
+  return { skip, limit }
+}
+
 exports.getAllTenants = async (req, res) => {
   try {
-    const queryObject = { ...req.query }
-    const excludeFileds = ['page', 'sort', 'limit', 'fields']
-
-    excludeFileds.forEach((field) => delete queryObject[field])
-    const queryString = JSON.parse(
-      JSON.stringify(queryObject).replace(
-        /(gt|gte|lt|gt)/g,
-        (match) => `$${match}`
-      )
-    )
+    const queryString = filter(req.query)
+    const sortBy = sort(req.query.sort)
+    const fields = chooseFields(req.query.fields)
+    const { skip, limit } = pagination(req.query)
 
     const query = Tenant.find(queryString)
+      .sort(sortBy)
+      .select(fields)
+      .skip(skip)
+      .limit(limit)
 
     const tenants = await query
 
@@ -51,6 +83,7 @@ exports.getAllTenants = async (req, res) => {
   } catch (err) {
     res.status(404).json({
       status: 'fail',
+      message: err,
       data: null
     })
   }
