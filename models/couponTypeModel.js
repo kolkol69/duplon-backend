9311abfc94937777536364ef8e3ebd8e8c8957bf
kolkol <kolodiy1103@gmail.com>
2020-11-15 21:41:08 +0100

    couponType model update

--- models/couponTypeModel.js
+++ models/couponTypeModel.js
@@ -1,9 +1,14 @@
 const mongoose = require('mongoose')
 
-const { Schema } = mongoose
+const { Schema, ObjectId } = mongoose
 const couponTypeSchema = new Schema({
+  tenantId: { type: ObjectId, require: true },
   html: { type: String },
-  val: { type: Number, require: true },
+  val: {
+    type: Number,
+    require: true,
+    min: [0, 'Discount cant be less than 0']
+  },
   title: { type: String, require: true },
   descr: { type: String, require: true }
 })
