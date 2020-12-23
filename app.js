bbac18c5c577b5352aae91f0e22f9ee86f62424d
kolkol <kolodiy1103@gmail.com>
2020-12-23 13:03:51 +0100

    add more security tweaks

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express')
-const cookieParser = require('cookie-parser')
+// const cookieParser = require('cookie-parser')
 const logger = require('morgan')
 const cors = require('cors')
-const bodyParser = require('body-parser')
+// const bodyParser = require('body-parser')
+const rateLimit = require('express-rate-limit')
+const mongoSanitize = require('express-mongo-sanitize')
+const xss = require('xss-clean')
+const helmet = require('helmet')
 const path = require('path')
 
 const AppError = require('./utils/appError')
@@ -17,21 +21,58 @@ require('./db/mongoose')
 
 const app = express()
 
+// 1) Global Middlewares
+
+// Set security HTTP headers
+app.use(helmet())
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(logger('dev'))
+}
+
+// Limit requests from the same IP
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!'
+})
+
+app.use('/api', limiter)
+
 app.use(cors())
-app.use(bodyParser.json()) // to support JSON-encoded bodies
+
+// app.use(bodyParser.json()) // to support JSON-encoded bodies
+// app.use(
+//   bodyParser.urlencoded({
+//     // to support URL-encoded bodies
+//     extended: true
+//   })
+// )
+
+// Body parser, reading data from body into req.body
 app.use(
-  bodyParser.urlencoded({
-    // to support URL-encoded bodies
-    extended: true
+  express.json({
+    limit: '10kb'
   })
 )
-app.use(logger('dev'))
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-app.use(cookieParser())
+
+// Data sanitization against NoSQL query injections
+app.use(mongoSanitize())
+
+// Data sanitization against XSS
+app.use(xss())
+
+// app.use(express.urlencoded({ extended: false }))
+// app.use(cookieParser())
+
+// Serving static files
 app.use(express.static(path.join(__dirname, 'public')))
+
+// Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString()
+  // console.log(req.headers)
+
   next()
 })
 
--- controllers/authController.js
+++ controllers/authController.js
@@ -5,7 +5,6 @@ const User = require('../models/userModel')
 const catchAsync = require('../utils/catchAsync')
 const AppError = require('../utils/appError')
 const sendEmail = require('../utils/email')
-const { findById } = require('../models/userModel')
 
 const signToken = (id) =>
   jwt.sign(
@@ -19,6 +18,22 @@ const signToken = (id) =>
   )
 const createSendToken = (user, statusCode, res) => {
   const token = signToken(user._id)
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true
+  }
+
+  if (process.env.NODE_ENV === 'production') {
+    cookieOptions.secure = true
+  }
+
+  res.cookie('jwt', token, cookieOptions)
+
+  // Remove the password from output
+  // eslint-disable-next-line no-param-reassign
+  user.password = undefined
 
   res.status(statusCode).json({
     status: 'success',
@@ -189,7 +204,8 @@ exports.updatePassword = catchAsync(async (req, res, next) => {
   const user = await User.findById(req.user.id).select('+password')
   // 2) Check if POSTed pass is correctPassword
   const correct =
-    user && (await user.correctPassword(req.body.passwordCurrent, user.password))
+    user &&
+    (await user.correctPassword(req.body.passwordCurrent, user.password))
 
   if (!correct) {
     return next(new AppError('Your current password is incorrect', 401))
