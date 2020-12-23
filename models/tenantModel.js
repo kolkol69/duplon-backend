fb9f807305db6fb72413d23601980ec16700291c
kolkol <kolodiy1103@gmail.com>
2020-12-23 22:11:19 +0100

    fix date

--- models/tenantModel.js
+++ models/tenantModel.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose')
 
 const tenantSchema = new mongoose.Schema({
   name: { type: String, required: true, unique: true },
-  planStart: { type: Date, default: Date(Date.now()) }, // when payed plan has be started
+  planStart: { type: Date, default: Date.now }, // when payed plan has be started
   activeStatus: { type: Boolean, default: false }, // if plan is active or not
-  joinDate: { type: Date, default: Date(Date.now()) },
+  joinDate: { type: Date, default: Date.now },
   usedAll: { type: Number, required: true, default: 0 }, // coupons used since joining
   usedLastPeriod: { type: Number, required: true, default: 0 } // coupons used for the last 30 days
 })
