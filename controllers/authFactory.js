bdd701102c446848b0f1879d69f352beffb2b92e
kolkol <kolodiy1103@gmail.com>
2020-12-27 20:56:17 +0100

    modify token position in response

--- controllers/authFactory.js
+++ controllers/authFactory.js
@@ -37,8 +37,7 @@ const createSendToken = (user, statusCode, res) => {
 
   res.status(statusCode).json({
     status: 'success',
-    token,
-    data: { user }
+    data: { user, token }
   })
 }
 
