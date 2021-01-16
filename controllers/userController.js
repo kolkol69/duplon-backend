07a8c605026e104b0154004d4735b53740e15c80
kolkol <kolodiy1103@gmail.com>
2021-01-16 22:15:36 +0100

    change return data structure

--- controllers/userController.js
+++ controllers/userController.js
@@ -54,7 +54,7 @@ exports.updateMe = catchAsync(async (req, res, next) => {
 
   res.status(200).json({
     status: 'success',
-    data: { user: updatedUser }
+    data: { data: updatedUser }
   })
 })
 
